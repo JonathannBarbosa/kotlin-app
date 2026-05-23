5aa6dde84760332fcfaede731a9f1170b867b830
Jonathan Barbosa <jonathanbazevedo@hotmail.com>
2026-05-23 18:40:22 -0300

    práticas com kotlin

---
+++ app/src/main/java/com/app/supercompras/MainActivity.kt
@@ -0,0 +1,158 @@
+package com.app.supercompras
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.app.supercompras.ui.theme.Marinho
+import com.app.supercompras.ui.theme.SuperComprasTheme
+import com.app.supercompras.ui.theme.Typography
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SuperComprasTheme() {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(verticalArrangement = Arrangement.Top) {
+                        ImagemTopo(modifier = Modifier.padding(innerPadding))
+                        Titulo(
+                            texto = "Lista de Compras",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                        ItemDaLista(modifier = Modifier.padding(innerPadding))
+                        Titulo(texto = "Comprado", modifier = Modifier.padding(innerPadding))
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Titulo(texto: String, modifier: Modifier = Modifier) {
+    Text(text = texto, style = Typography.headlineLarge, modifier = modifier)
+}
+
+@Composable
+fun ItemDaLista(modifier: Modifier = Modifier) {
+    Column(verticalArrangement = Arrangement.Top, modifier = modifier) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Checkbox(
+                checked = false,
+                onCheckedChange = {},
+                modifier = Modifier
+                    .padding(end = 8.dp)
+                    .requiredSize(24.dp)
+            )
+            Text(
+                text = "Suco",
+                modifier = Modifier.weight(1f),
+                style = Typography.bodyMedium,
+                textAlign = TextAlign.Start
+            )
+            Icone(
+                Icons.Default.Delete,
+                modifier = Modifier
+                    .padding(end = 8.dp)
+                    .size(16.dp)
+            )
+            Icone(Icons.Default.Edit, modifier = Modifier.size(16.dp))
+        }
+        Text(
+            "Segunda-feira (31/10/2022) às 08:30",
+            Modifier.padding(top = 8.dp),
+            style = Typography.labelSmall,
+        )
+
+    }
+}
+
+@Composable
+fun ImagemTopo(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(R.drawable.image_topo),
+        contentDescription = null,
+        modifier = modifier.size(160.dp)
+    )
+}
+
+@Composable
+fun Icone(icone: ImageVector, modifier: Modifier = Modifier) {
+    Icon(icone, contentDescription = "Editar", modifier = modifier, tint = Marinho)
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello Fernando",
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+fun ItemDaListaPreview() {
+    SuperComprasTheme {
+        ItemDaLista()
+    }
+}
+
+@Preview
+@Composable
+fun IconeEditPreview() {
+    SuperComprasTheme {
+        Icone(icone = Icons.Default.Delete)
+    }
+}
+
+@Preview
+@Composable
+fun ImagemTopoPreview() {
+    SuperComprasTheme {
+        ImagemTopo()
+    }
+}
+
+@Preview
+@Composable
+fun TituloPreview() {
+    SuperComprasTheme {
+        Titulo(texto = "Lista de Compras")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SuperComprasTheme {
+        Greeting("Android")
+    }
+}
