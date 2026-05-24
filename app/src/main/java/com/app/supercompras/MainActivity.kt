2395f92598817b18938f4c7e43ede96d5baabbe6
Jonathan Barbosa <jonathanbazevedo@hotmail.com>
2026-05-24 13:33:42 -0300

    feat: config para ação dos botões, gerenciar estados

--- app/src/main/java/com/app/supercompras/MainActivity.kt
+++ app/src/main/java/com/app/supercompras/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,10 +19,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -43,6 +47,10 @@ import androidx.compose.ui.unit.dp
 import com.app.supercompras.ui.theme.Marinho
 import com.app.supercompras.ui.theme.SuperComprasTheme
 import com.app.supercompras.ui.theme.Typography
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,28 +76,95 @@ fun ListaDeCompras(modifier: Modifier = Modifier) {
         modifier = modifier
     ) {
         ImagemTopo()
-        AdicionarItem(aoSalvarItem = { textNovo ->
-            listaDeItens = listaDeItens + ItemCompra(textNovo)
+        AdicionarItem(aoSalvarItem = { novoItem ->
+            listaDeItens = listaDeItens + novoItem
         })
         Spacer(modifier = Modifier.height(48.dp))
         Titulo(
             texto = "Lista de Compras",
         )
-        Column {
-            listaDeItens.forEach { item ->
-                ItemDaLista(item)
+        ListaDeItems(
+            lista = listaDeItens.filter { !it.foiComprado },
+            aoMudarStatus = { itemSelecionado ->
+                listaDeItens = listaDeItens.map { itemMap ->
+                    if (itemSelecionado == itemMap) {
+                        itemSelecionado.copy(foiComprado = !itemSelecionado.foiComprado)
+                    } else {
+                        itemMap
+                    }
+                }
+            },
+            aoRemoverItem = { itemRemovido ->
+                listaDeItens = listaDeItens - itemRemovido
+            },
+            aoEditarItem = { itemEditado, novoTexto ->
+                listaDeItens = listaDeItens.map { itemAtual ->
+                    if (itemAtual == itemEditado) {
+                        itemAtual.copy(texto = novoTexto)
+                    } else {
+                        itemAtual
+                    }
+                }
             }
-        }
+        )
+
         Titulo(texto = "Comprado")
+
+        if (listaDeItens.any { it.foiComprado }) {
+            ListaDeItems(
+                lista = listaDeItens.filter { it.foiComprado },
+                aoMudarStatus = { itemSelecionado ->
+                    listaDeItens = listaDeItens.map { itemMap ->
+                        if (itemSelecionado == itemMap) {
+                            itemSelecionado.copy(foiComprado = !itemSelecionado.foiComprado)
+                        } else {
+                            itemMap
+                        }
+                    }
+                },
+                aoRemoverItem = { itemRemovido ->
+                    listaDeItens = listaDeItens - itemRemovido
+                },
+                aoEditarItem = { itemEditado, novoTexto ->
+                    listaDeItens = listaDeItens.map { itemAtual ->
+                        if (itemAtual == itemEditado) {
+                            itemAtual.copy(texto = novoTexto)
+                        } else {
+                            itemAtual
+                        }
+                    }
+                }
+            )
+        }
     }
 }
 
 @Composable
-fun AdicionarItem(aoSalvarItem: (texto: String) -> Unit, modifier: Modifier = Modifier) {
-    var texto = rememberSaveable { mutableStateOf("") }
+fun ListaDeItems(
+    lista: List<ItemCompra>,
+    aoMudarStatus: (item: ItemCompra) -> Unit = {},
+    aoRemoverItem: (item: ItemCompra) -> Unit = {},
+    aoEditarItem: (item: ItemCompra, novoTexto: String) -> Unit = {_, _ ->},
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        lista.forEach { item ->
+            ItemDaLista(
+                item = item,
+                aoMudarStatus = aoMudarStatus,
+                aoRemoverItem = aoRemoverItem,
+                aoEditarItem = aoEditarItem
+            )
+        }
+    }
+}
+
+@Composable
+fun AdicionarItem(aoSalvarItem: (item: ItemCompra) -> Unit, modifier: Modifier = Modifier) {
+    var texto by rememberSaveable { mutableStateOf("") }
     OutlinedTextField(
-        value = texto.value,
-        onValueChange = { texto.value = it },
+        value = texto,
+        onValueChange = { texto = it },
         placeholder = {
             Text(
                 text = "Digite o item que deseja adicionar",
@@ -107,8 +182,8 @@ fun AdicionarItem(aoSalvarItem: (texto: String) -> Unit, modifier: Modifier = Mo
     Button(
         shape = RoundedCornerShape(24.dp),
         onClick = {
-            aoSalvarItem(texto.value)
-            texto.value = ""
+            aoSalvarItem(ItemCompra(texto, false, getDataHora()))
+            texto = ""
         },
         modifier = modifier
     ) {
@@ -121,45 +196,91 @@ fun AdicionarItem(aoSalvarItem: (texto: String) -> Unit, modifier: Modifier = Mo
     }
 }
 
+fun getDataHora(): String {
+    val dataHoraAtual = System.currentTimeMillis()
+    val dataHoraFormata = SimpleDateFormat("EEEE (dd/MM/yyyy) 'às' HH:mm", Locale("pt", "BR"))
+    return dataHoraFormata.format(dataHoraAtual)
+}
+
 @Composable
 fun Titulo(texto: String, modifier: Modifier = Modifier) {
     Text(text = texto, style = Typography.headlineLarge, modifier = modifier)
 }
 
 @Composable
-fun ItemDaLista(item: ItemCompra, modifier: Modifier = Modifier) {
+fun ItemDaLista(
+    item: ItemCompra,
+    aoMudarStatus: (item: ItemCompra) -> Unit = {},
+    aoRemoverItem: (item: ItemCompra) -> Unit = {},
+    aoEditarItem: (item: ItemCompra, novoTexto: String) -> Unit = {_, _ -> },
+    modifier: Modifier = Modifier
+) {
     Column(verticalArrangement = Arrangement.Top, modifier = modifier) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start
         ) {
+            var textoEditado by rememberSaveable { mutableStateOf(item.texto) }
+            var edicao by rememberSaveable { mutableStateOf(false) }
+
             Checkbox(
-                checked = false,
-                onCheckedChange = {},
+                checked = item.foiComprado,
+                onCheckedChange = {
+                    aoMudarStatus(item)
+                },
                 modifier = Modifier
                     .padding(end = 8.dp)
                     .requiredSize(24.dp)
             )
-            Text(
-                text = item.texto,
-                modifier = Modifier.weight(1f),
-                style = Typography.bodyMedium,
-                textAlign = TextAlign.Start
-            )
-            Icone(
-                Icons.Default.Delete,
-                modifier = Modifier
-                    .padding(end = 8.dp)
-                    .size(16.dp)
-            )
-            Icone(Icons.Default.Edit, modifier = Modifier.size(16.dp))
+
+            if (edicao) {
+                OutlinedTextField(
+                    value = textoEditado,
+                    onValueChange = { textoEditado = it },
+                    modifier = Modifier.weight(1f),
+                    singleLine = true,
+                    shape = RoundedCornerShape(24.dp)
+                )
+
+                IconButton(
+                    onClick = {
+                        aoEditarItem(item, textoEditado)
+                        edicao = false
+                    }
+                ) {
+                    Icone(Icons.Default.Done, modifier = Modifier.size(16.dp))
+                }
+            } else {
+                Text(
+                    text = item.texto,
+                    modifier = Modifier.weight(1f),
+                    style = Typography.bodyMedium,
+                    textAlign = TextAlign.Start
+                )
+            }
+            IconButton(
+                onClick = { aoRemoverItem(item) },
+                modifier = Modifier.padding(end = 8.dp)
+            ) {
+                Icone(
+                    Icons.Default.Delete,
+                    modifier = Modifier
+                        .size(16.dp)
+                )
+            }
+            IconButton(
+                onClick = {
+                    edicao = true
+                }
+            ) {
+                Icone(Icons.Default.Edit, modifier = Modifier.size(16.dp))
+            }
         }
         Text(
-            "Segunda-feira (31/10/2022) às 08:30",
+            item.datahora,
             Modifier.padding(top = 8.dp),
             style = Typography.labelSmall,
         )
-
     }
 }
 
@@ -197,7 +318,7 @@ private fun AdicionarItemPreview() {
 @Composable
 fun ItemDaListaPreview() {
     SuperComprasTheme {
-        ItemDaLista(item = ItemCompra("Suco"))
+        ItemDaLista(item = ItemCompra("Suco", false, "Segunda-feira"))
     }
 }
 
@@ -234,5 +355,7 @@ fun GreetingPreview() {
 }
 
 data class ItemCompra(
-    val texto: String
+    val texto: String,
+    var foiComprado: Boolean = false,
+    val datahora: String
 )
